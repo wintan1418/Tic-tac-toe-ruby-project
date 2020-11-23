1ee0e3beb2556ae8a6bf0c9e964bcd5f22c0a135
KinezaEnaelle <kiris2024@gmail.com>
2020-11-23 17:15:54 +0100

    Add player

---
+++ main.rb
@@ -0,0 +1,14 @@
+def name
+end
+def symbol
+
+def player_name
+    puts "Please enter your name"
+    player1 = gets.chomp()
+    player1 = name
+    puts "Please enter your name"
+    player2 = gets.chomp()
+    player2 = name
+end
+def player_symbol
+end
