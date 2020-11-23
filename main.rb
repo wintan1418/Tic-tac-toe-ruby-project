b08aa6d934614dffd9e307e9a53e09e64185860e
KinezaEnaelle <kiris2024@gmail.com>
2020-11-23 22:02:49 +0100

    Fix linters

--- main.rb
+++ main.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 
 class Player
-    attr_accessor :name, :key
-    def initialize(name,key)
-        @name = name
-        @key = key
-    end
+  attr_accessor :name, :key
+  def initialize(name, key)
+    @name = name
+    @key = key
+  end
 end
 
 def player_init
-        puts "Enter the name of the first player"
-        @player1.name = gets.chomp()
-        puts "Enter the name of the second player"
-        @player2.name = gets.chomp()
+  puts 'Enter the name of the first player'
+  @player1.name = gets.chomp
+  puts 'Enter the name of the second player'
+  @player2.name = gets.chomp
 end
- p.Player.new
- p.player_init()
- puts "Now, your move is displayed on the board"
+p.Player.new
+p.player_init
+puts 'Now, your move is displayed on the board'
